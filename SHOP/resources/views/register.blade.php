76568cc96a559ce6ca281e26c23bef284e769682
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2025-06-18 15:22:35 +0700

    resgister

--- SHOP/resources/views/register.blade.php
+++ SHOP/resources/views/register.blade.php
@@ -21,27 +21,72 @@
 
             <div class="header_menu">
                 <ul class="nav_menu">
-                    <li class="nav_item">
-                        <a href="">HÀNG MỚI VỀ</a>
-                    </li>
-                    <li class="nav_item has_submenu">
-                        <a href="">SẢN PHẦM
+                    <li class="nav_item has_submenu has_submenu_product">
+                        <a href="{{ route('lietke') }}">SẢN PHẦM
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" x="0" y="0" viewBox="0 0 128 128"><g><path d="m64 88c-1.023 0-2.047-.391-2.828-1.172l-40-40c-1.563-1.563-1.563-4.094 0-5.656s4.094-1.563 5.656 0l37.172 37.172 37.172-37.172c1.563-1.563 4.094-1.563 5.656 0s1.563 4.094 0 5.656l-40 40c-.781.781-1.805 1.172-2.828 1.172z"></path></g></svg>
                         </a>
+                        <div class="base_product"></div>
+                        <div class="menu_list_warapper">
+                            <div class="container_wrapper">
+                                <div class="product_wrapper">
+                                    <div class="product_infor">
+                                        <img src="https://file.hstatic.net/200000000133/file/ao.jpg" alt="" class="product_img">
+                                        <span>Aó</span>
+                                    </div>
+
+                                    <div class="product_infor">
+                                        <img src="https://file.hstatic.net/200000000133/file/dam.jpg" alt="" class="product_img">
+                                        <span>Đầm</span>
+                                    </div>
+
+                                    <div class="product_infor">
+                                        <img src="https://file.hstatic.net/200000000133/file/chan_vay.jpg" alt="" class="product_img">
+                                        <span>Chân váy</span>
+                                    </div>
+
+                                    <div class="product_infor">
+                                        <img src="https://file.hstatic.net/200000000133/file/quan.jpg" alt="" class="product_img">
+                                        <span>Quần</span>
+                                    </div>
+
+                                    <div class="product_infor">
+                                        <img src="https://file.hstatic.net/200000000133/file/img_megamenu2_5_a5bce29661434ffdad3e4cbe7e5540ea.jpg" alt="" class="product_img">
+                                        <span>Jumpsuit</span>
+                                    </div>
+
+                                    <div class="product_infor">
+                                        <img src="https://file.hstatic.net/200000000133/file/ao_khaocs.jpg" alt="" class="product_img">
+                                        <span>Aó Khoác</span>
+                                    </div>
+
+                                    <div class="product_infor">
+                                        <img src="https://file.hstatic.net/200000000133/file/phu_kien.jpg" alt="" class="product_img">
+                                        <span>Phụ Kiện</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </li>
                     <li class="nav_item">
-                        <a href="">BỘ SƯU TẬP</a>
+                        <a href="{{ route('products.byType', ['type' => 'ao']) }}">ÁO</a>
                     </li>
                     <li class="nav_item">
-                        <a href="">CASAUL</a>
+                        <a href="{{ route('products.byType', ['type' => 'dam']) }}">ĐẦM</a>
                     </li>
                     <li class="nav_item">
-                        <a href="">LADY ME</a>
+                        <a href="{{ route('products.byType', ['type' => 'chan-vay']) }}">CHÂN VÁY</a>
                     </li>
                     <li class="nav_item has_submenu">
-                        <a href="">SALE
-                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" x="0" y="0" viewBox="0 0 128 128"><g><path d="m64 88c-1.023 0-2.047-.391-2.828-1.172l-40-40c-1.563-1.563-1.563-4.094 0-5.656s4.094-1.563 5.656 0l37.172 37.172 37.172-37.172c1.563-1.563 4.094-1.563 5.656 0s1.563 4.094 0 5.656l-40 40c-.781.781-1.805 1.172-2.828 1.172z"></path></g></svg>
-                        </a>
+                        <a href="{{ route('products.byType', ['type' => 'quan']) }}">QUẦN</a>
+                    </li>
+                    <li class="nav_item has_submenu">
+                        <a href="{{ route('products.byType', ['type' => 'jumpsuit']) }}">JUMPSUIT</a>
+                    </li>
+                    <li class="nav_item has_submenu">
+                        <a href="{{ route('products.byType', ['type' => 'ao-khoac']) }}">ÁO KHOÁC</a>
+                    </li>
+                    <li class="nav_item has_submenu">
+                        <a href="{{ route('products.byType', ['type' => 'phu-kien']) }}">PHỤ KIỆN</a>
                     </li>
                 </ul>
             </div>
@@ -73,84 +118,8 @@
             <h1 >
                 <a href="{{ route('register') }}" style="text-decoration: none;">REGISTER</a>
             </h1>
-            <?php 
-                $customerName = $email = $password = $re_password = $date = "";
-                $customerNameErr = $emailErr = $passwordErr = $re_passwordErr = "";
-                if($_SERVER["REQUEST_METHOD"] == "POST"){
-                    // CHECK TEN
-                    if(empty($_POST["customerName"])){
-                        $customerNameErr = "Yêu cầu nhập tên";
-                    }else{
-                        $customerName = test_input($_POST["customerName"]);
-                        if (!preg_match("/^[\p{L}\s'-]+$/u", $customerName)) {
-                            $customerNameErr = "Chỉ cho phép chữ cái và khoảng trắng";
-                        }
-                    }
-                    // check mk
-                    if(empty($_POST["email"])){
-                        $emailErr = "Yêu cầu nhập email";
-                    }else{
-                        $email = test_input($_POST["email"]);
-                        if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-                            $emailErr = "Lỗi format";
-                        }
-                    }
-
-
-
-                    if(empty($_POST["password"])){
-                        $passwordErr = "Xin nhập mật khẩu";
-                    } else {
-                        $password = test_input($_POST["password"]);
-                        $validationErrors = validate_password($password);
-                        if(!empty($validationErrors)){
-                            $passwordErr = implode("<br>", $validationErrors);
-                        } else {
-                            $passwordErr = ""; // no error
-                        }
-                    }
-                    
-                    
-                    
-                    
-
-
-                    // kiểm tra pass
-                    $re_password = test_input($_POST["re_password"]);
-
-                    if(empty($re_password)){
-                        $re_passwordErr = "Vui lòng nhập lại mật khẩu";
-                    } elseif($password !== $re_password){
-                        $re_passwordErr = "Mật khẩu không khớp";
-                    }
-                    if(empty($_POST["date"])){
-                        $date = "";
-                    }else{
-                        $date = test_input($_POST["date"]);
-                    }           
-                }
-                function test_input($data){
-                    $data = trim($data);
-                    $data = stripslashes($data);
-                    $data = htmlspecialchars($data);
-                    return $data;
-                }
-                function validate_password($password){
-                    $erros = [];
-                    if(strlen(($password) < 8)){
-                        $erros[] = "Mật khẩu có ít nhất 8 ký tự";
-                    }elseif(!preg_match("/[A-Z]/", $password)){
-                        $erros[] ="Mật khẩu phải có chữ in hoa";
-                    }elseif(!preg_match("/[a-z]/", $password)){
-                        $erros[] ="Mật khẩu ít nhất có chứa 1 chữ thường";
-                    }elseif(!preg_match("/[0-9]/", $password)){
-                        $erros[] ="Mật khẩu phải chứa số";
-                    }elseif(!preg_match("/[\W_]/", $password)){
-                        $erros[] ="Mật khẩu phải chứa ký tự đặc biệt";
-                    }
-                    return $erros;
-                }
-            ?>
+           
+           
             <form action="{{ route("register.submit") }}" method="post">
                 @csrf {{-- THÊM DÒNG NÀY --}}
                 <input type="text" placeholder="Họ và tên:" name="customerName" class="input_sub" autocomplete="off">
